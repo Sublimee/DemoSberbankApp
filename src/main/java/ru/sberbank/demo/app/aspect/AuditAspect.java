d09fc30b64d2a654ce3934597405a04ecfc9f91d
Maksim Gusev <aquariusborelias@gmail.com>
2020-03-11 18:44:12 +0300

    simple aspect audit added

---
+++ src/main/java/ru/sberbank/demo/app/aspect/AuditAspect.java
@@ -0,0 +1,21 @@
+package ru.sberbank.demo.app.aspect;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class AuditAspect {
+
+    private static final Logger logger = LoggerFactory.getLogger(AuditAspect.class);
+
+    @Before("execution(* ru.sberbank.demo.app.repository.AccountsRepository.getAccountsByClientId(Long))")
+    public void before(JoinPoint joinPoint) {
+        logger.info("Searching Project with id {}", joinPoint.getArgs()[0]);
+    }
+
+}
