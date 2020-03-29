ec81cb393258ba1a8f3e19f06c9468de488204ab
Maksim Gusev <aquariusborelias@gmail.com>
2020-03-29 14:44:42 +0300

    @Slf4j added to AuditAspect.java

--- src/main/java/ru/sberbank/demo/app/aspect/AuditAspect.java
+++ src/main/java/ru/sberbank/demo/app/aspect/AuditAspect.java
@@ -1,21 +1,19 @@
 package ru.sberbank.demo.app.aspect;
 
+import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
+@Slf4j
 public class AuditAspect {
-
-    private static final Logger LOGGER = LoggerFactory.getLogger(AuditAspect.class);
 
     @Before("execution(* ru.sberbank.demo.app.repository.AccountsRepository.getAccountsByClientId(Long))")
     public void auditClientAccounts(final JoinPoint joinPoint) {
-        LOGGER.info("Searching Project with id {}", joinPoint.getArgs()[0]);
+        log.info("Searching Project with id {}", joinPoint.getArgs()[0]);
     }
 
 }
